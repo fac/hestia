7f42dbebf936fe0c6ac357b19affb97e4b9a28ca
Caius Durling <dev@caius.name>
2014-12-19 12:07:35 +0000

    Add Hestia::MessageMultiVerifier

---
+++ lib/hestia/message_multi_verifier.rb
@@ -0,0 +1,71 @@
+require "active_support/message_verifier"
+
+module Hestia
+  class MessageMultiVerifier
+
+    # Public: creates a verifier for verifying against multiple secrets
+    #
+    # API compatible with ActiveSupport::MessageVerifier once created. (initializer arguments are different.)
+    #
+    # current_secret: [String] The current secret token, used for signing *and* verifying cookies
+    # deprecated_secrets: [String, Array] The previous secret token(s), used for verifying cookies only
+    # options [Hash] options to be passed to the verifiers - see ActiveSupport::MessageVerifier#initialze for details
+    #
+    def initialize(current_secret:, deprecated_secrets: [], options: {})
+      @current_verifier = build_verifier(current_secret, options)
+      @deprecated_verifiers = Array(deprecated_secrets).map { |secret| build_verifier(secret, options) }.freeze
+
+      # Generate these here so they are static when accessed in #verify
+      @verifiers = [current_verifier, *deprecated_verifiers].freeze
+
+      # Don't need to change any attributes, so why stay mutable?
+      freeze
+    end
+
+    # Public: generate a signed message
+    #
+    # Uses only the `current_secret` to generate this.
+    #
+    # See ActiveSupport::MessageVerifier#generate for more information
+    #
+    # Returns String
+    def generate(value)
+      current_verifier.generate(value)
+    end
+
+    # Public: verify a signed message and convert back to original form
+    #
+    # See ActiveSupport::MessageVerifier#verify for more information
+    #
+    # Returns deserialized value
+    # Raises ActiveSupport::MessageVerifier::InvalidSignature
+    def verify(signed_message)
+      errored_verifier_count = 0
+
+      # Make sure we check *all* verifiers, every time we're called, to prevent timing attacks.
+      # We run in a consistent amount of time no matter which (or if any) verifiers return a valid result
+      results = verifiers.each_with_object([]) do |verifier, values|
+        begin
+          values << verifier.verify(signed_message)
+        rescue ActiveSupport::MessageVerifier::InvalidSignature
+          errored_verifier_count += 1
+        end
+      end
+
+      # If all the verifiers errored, then none of them thought the message was valid
+      if errored_verifier_count == verifiers.size
+        raise(ActiveSupport::MessageVerifier::InvalidSignature)
+      else
+        results.first
+      end
+    end
+
+    private
+
+    attr_reader :current_verifier, :deprecated_verifiers, :verifiers
+
+    def build_verifier(secret, options)
+      ActiveSupport::MessageVerifier.new(secret, options)
+    end
+  end
+end
---
+++ spec/hestia/message_multi_verifier_spec.rb
@@ -0,0 +1,140 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "minitest/spec"
+
+require_relative "../../lib/hestia/message_multi_verifier"
+
+module Hestia
+  describe MessageMultiVerifier do
+    InvalidSignature = ActiveSupport::MessageVerifier::InvalidSignature
+
+    let(:message_verifier) { ActiveSupport::MessageVerifier.new("secret") }
+    let(:singular_verifier) { MessageMultiVerifier.new(current_secret: "secret") }
+    let(:multi_verifier) { MessageMultiVerifier.new(current_secret: "secret", deprecated_secrets: %w(previous_secret)) }
+
+    describe "creation" do
+      it "requires a current secret" do
+        -> { MessageMultiVerifier.new }.must_raise(ArgumentError, /current_secret/)
+      end
+
+      it "can be created with current & deprecated secrets" do
+        MessageMultiVerifier.new(current_secret: "secret", deprecated_secrets: %w(previous_secret legacy_secret))
+      end
+
+      it "can be created with current secret & options only" do
+        MessageMultiVerifier.new(current_secret: "secret", options: {digest: "MD5"})
+      end
+
+      it "can be created with array of secrets and options for the verifiers" do
+        MessageMultiVerifier.new(current_secret: "secret", deprecated_secrets: %w(previous_secret), options: {digest: "MD5"})
+      end
+
+      it "is immutable after creation" do
+        multi_verifier.must_be :frozen?
+      end
+    end
+
+    describe "generating messages with one secret set" do
+      it "should be generated using first secret" do
+        singular_verifier.generate("cookie dough").must_equal message_verifier.generate("cookie dough")
+      end
+    end
+
+    describe "generating messages with multiple secrets set" do
+      it "should be generated using first secret" do
+        multi_verifier.generate("cookie dough").must_equal message_verifier.generate("cookie dough")
+      end
+    end
+
+    describe "verify behaviour" do
+      let(:legacy_message_verifier) { ActiveSupport::MessageVerifier.new("previous_secret") }
+
+      let(:current_cookie) { message_verifier.generate("cookie dough") }
+      let(:legacy_cookie) { legacy_message_verifier.generate("cookie dough") }
+
+      it "should error with blank message" do
+        -> { multi_verifier.verify(nil) }.must_raise(InvalidSignature)
+        -> { singular_verifier.verify("") }.must_raise(InvalidSignature)
+
+        -> { multi_verifier.verify(nil) }.must_raise(InvalidSignature)
+        -> { multi_verifier.verify("") }.must_raise(InvalidSignature)
+      end
+
+      it "should error without -- delimited data/digest" do
+        invalid_cookie = current_cookie.gsub("--", "-")
+        -> { singular_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+        -> { multi_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+      end
+
+      it "should error without data being present" do
+        invalid_cookie = current_cookie.gsub(/\A.+--/, "")
+        -> { singular_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+        -> { multi_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+      end
+
+      it "should error without digest being present" do
+        invalid_cookie = current_cookie.gsub(/--.+\z/, "")
+        -> { singular_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+        -> { multi_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+      end
+
+      it "should error without passed in digest matching computed digest" do
+        invalid_cookie = current_cookie.gsub(/^.+--/, "lol_error_not_a_digest_zomg")
+        -> { singular_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+        -> { multi_verifier.verify(invalid_cookie) }.must_raise(InvalidSignature)
+      end
+
+      it "should error with cookie signed using unknown secret" do
+        -> { singular_verifier.verify(legacy_cookie) }.must_raise(InvalidSignature)
+      end
+
+      it "should verify successfully with correct payload for any valid secret" do
+        singular_verifier.verify(current_cookie).must_equal "cookie dough"
+
+        multi_verifier.verify(current_cookie).must_equal "cookie dough"
+        multi_verifier.verify(legacy_cookie).must_equal "cookie dough"
+      end
+
+      it "should verify a message of `nil' successfully" do
+        nil_cookie = singular_verifier.generate(nil)
+
+        singular_verifier.verify(nil_cookie).must_equal(nil)
+        multi_verifier.verify(nil_cookie).must_equal(nil)
+      end
+
+      it "should verify successfully when using custom digest" do
+        multi_singular_secret = MessageMultiVerifier.new(current_secret: "secret", options: {digest: "MD5"})
+        multi_multiple_secret = MessageMultiVerifier.new(current_secret: "secret", deprecated_secrets: %w(previous_secret), options: {digest: "MD5"})
+
+        current_cookie = ActiveSupport::MessageVerifier.new("secret", :digest => "MD5").generate("cookie dough")
+        previous_cookie = ActiveSupport::MessageVerifier.new("previous_secret", :digest => "MD5").generate("cookie dough")
+
+        multi_singular_secret.verify(current_cookie).must_equal "cookie dough"
+        multi_multiple_secret.verify(current_cookie).must_equal "cookie dough"
+        multi_multiple_secret.verify(previous_cookie).must_equal "cookie dough"
+      end
+
+      it "should verify successfully when using custom serializer" do
+        reverser = Module.new do
+          extend self
+
+          def dump(string)
+            string.reverse
+          end
+
+          alias_method :load, :dump
+        end
+
+        multi_singular_secret = MessageMultiVerifier.new(current_secret: "secret", options: {serializer: reverser})
+        multi_multiple_secret = MessageMultiVerifier.new(current_secret: "secret", deprecated_secrets: %w(previous_secret), options: {serializer: reverser})
+
+        current_cookie = ActiveSupport::MessageVerifier.new("secret", :serializer => reverser).generate("cookie dough")
+        previous_cookie = ActiveSupport::MessageVerifier.new("previous_secret", :serializer => reverser).generate("cookie dough")
+
+        multi_singular_secret.verify(current_cookie).must_equal "cookie dough"
+        multi_multiple_secret.verify(current_cookie).must_equal "cookie dough"
+        multi_multiple_secret.verify(previous_cookie).must_equal "cookie dough"
+      end
+    end
+  end
+end
