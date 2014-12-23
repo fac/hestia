5b2edc703278b9f70e55f5bf1c4e623bcac99504
Caius Durling <dev@caius.name>
2014-12-23 12:12:48 +0000

    Autoload constants in the right files

--- lib/hestia.rb
+++ lib/hestia.rb
@@ -1,5 +1,5 @@
-require "hestia/version"
-
 module Hestia
   autoload :MessageMultiVerifier, "hestia/message_multi_verifier"
+  autoload :SignedCookieJarExtension, "hestia/signed_cookie_jar_extension"
+  autoload :VERSION, "hestia/version"
 end
