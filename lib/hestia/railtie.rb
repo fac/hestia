67ab32c169ae38aaf562804377e4454730469972
Caius Durling <dev@caius.name>
2015-07-09 13:09:35 +0100

    Load correct extension for rails version

--- lib/hestia/railtie.rb
+++ lib/hestia/railtie.rb
@@ -7,7 +7,16 @@ module Hestia
     # See README.md for how to configure this in your application.
     #
     initializer "hestia.signed_cookie_jar_extension", before: :load_config_initializers do
-      ActionDispatch::Cookies::SignedCookieJar.prepend(Hestia::SignedCookieJarExtension)
+      extension = case ActionPack::VERSION::MAJOR
+      when 3
+        Hestia::SignedCookieJarExtension::ActionPack3
+      when 4
+        Hestia::SignedCookieJarExtension::ActionPack4
+      else
+        raise "Unsupported version of action_pack: #{ActionPack::VERSION::STRING.inspect}"
+      end
+
+      ActionDispatch::Cookies::SignedCookieJar.prepend(extension)
     end
   end
 end
